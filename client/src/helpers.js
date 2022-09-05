d9b4aade2b2d248e0b88f731f216466672b96a81
aPruner <adampruner99@gmail.com>
2022-09-05 12:39:38 -0700

    format things with prettier, will install it to the project as dev dependency soon

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -2,26 +2,23 @@ const DEFAULT_CHARACTER_STATS = {
   HP: 120,
   ATK: 10,
   DEF: 10,
-  SPD: 10
-};
-
+  SPD: 10,
+}
 
 // TODO: Make this return a random character
 export function generateCharacter(name) {
- return {
-  name,
-  ...DEFAULT_CHARACTER_STATS
- }
-};
+  return {
+    name,
+    ...DEFAULT_CHARACTER_STATS,
+  }
+}
 
 export function renderCharInfo(character) {
   if (!character) {
-    return;
+    return
   }
 
   return Object.keys(character).map((attribute, i) => {
-    return (
-      <p key={i}>{`${attribute}:${character[attribute]}`}</p>
-    )
+    return <p key={i}>{`${attribute}:${character[attribute]}`}</p>
   })
-};
+}
